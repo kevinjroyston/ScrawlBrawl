48322b3dddb46636f44dfeff1d6c4f42d1ecf8c8
Kevin Royston <kevin@royston.com>
2020-01-15 23:47:53 -0800

    saving before deleting monogame

---
+++ TV/LobbyManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoystonGame.TV
+{
+    public class LobbyManager
+    {
+        public LobbyManager(RunGame gameRunner)
+        {
+            if (Singleton == null)
+            {
+                Singleton = this;
+            }
+            else
+            {
+                throw new Exception("Tried to instantiate a second instance of GameManager.cs");
+            }
+
+            this.GameRunner = gameRunner;
+        }
+    }
+}
