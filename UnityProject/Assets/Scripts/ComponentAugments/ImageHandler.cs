f64365ed8cc0d1feedc9163dd51440b4df2a4751
Kevin Royston <kevin@royston.com>
2020-04-24 18:49:57 -0700

    WIP new unity client

--- UnityProject/Assets/Scripts/ComponentAugments/ImageHandler.cs
+++ UnityProject/Assets/Scripts/ComponentAugments/ImageHandler.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 
 public class ImageHandler : MonoBehaviour
 {
-    public Image Image;
+    private List<Image> Images = new List<Image>();
+    public GameObject SubImagePrefab;
     public Image Background;
     public GameObject VoteCountHolder;
+    public GameObject SubImageHolder;
     public Text VoteCount;
     public Text Title;
     public Text Header;
@@ -17,18 +19,42 @@ public class ImageHandler : MonoBehaviour
     {
         set
         {
-            // TODO: multi-sprites.
-            Sprite sprite = value?.PngSprites?.FirstOrDefault();
-
             Background.preserveAspect = true;
-            Image.preserveAspect = true;
 
-            Image.sprite = sprite;
-            Image.color = sprite == null ? new Color(0f, 0f, 0f, 0f) : Color.white;
+            // Default to invisible background, overridden if subimages present.
+            Background.color = new Color(0f, 0f, 0f, 0f);
 
-            // TODO: find better option than creating new sprite for getting aspect ratio correct.
-            Background.sprite = Sprite.Create(new Texture2D((int)sprite.rect.width, (int)sprite.rect.height), sprite.rect, sprite.pivot, 10f, 0, SpriteMeshType.FullRect);
-            Background.color = sprite == null ? new Color(0f, 0f, 0f, 0f) : value?._BackgroundColor?.ToColor() ?? Color.white;
+            if (value?.PngSprites != null)
+            {
+                for (int i = 0; i < value.PngSprites.Count; i++)
+                {
+                    if (Images.Count <= i)
+                    {
+                        var subImage = GameObject.Instantiate(SubImagePrefab);
+                        subImage.transform.SetParent(SubImageHolder.transform);
+                        Images.Add(subImage.GetComponent<Image>());
+                    }
+                    Image image = Images[i];
+                    Sprite sprite = value.PngSprites[i];
+
+                    // Set background if we have any sub images.
+                    if (i == 0)
+                    {
+                        Background.sprite = Sprite.Create(new Texture2D((int)sprite.rect.width, (int)sprite.rect.height), sprite.rect, sprite.pivot, 10f, 0, SpriteMeshType.FullRect);
+                        Background.color = value?._BackgroundColor?.ToColor() ?? Color.white;
+                    }
+
+                    image.preserveAspect = true;
+                    image.sprite = value.PngSprites[i];
+                    image.color = value.PngSprites[i] == null ? new Color(0f, 0f, 0f, 0f) : Color.white;
+                }
+            }
+
+            // Make any extra sub images invisible
+            for (int i = value?.PngSprites?.Count ?? 0; i < Images.Count; i++)
+            {
+                Images[i].color = new Color(0f, 0f, 0f, 0f);
+            }
 
             Title.text = value?._Title ?? string.Empty;
             Title.enabled = value?._Title != null;
