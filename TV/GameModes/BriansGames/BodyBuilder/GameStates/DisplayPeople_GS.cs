d2448796bcba66db50e354aa46a8537c2591c5e6
BrianRoyston <60642573+BrianRoyston@users.noreply.github.com>
2020-05-07 21:40:43 -0700

    small changes and testing (#12)
    * small changes and testing
    
    * Fixed BodyBuilder's level transitions
    
    * added new level transition thing
    
    * added commented changes

--- TV/GameModes/BriansGames/BodyBuilder/GameStates/DisplayPeople_GS.cs
+++ TV/GameModes/BriansGames/BodyBuilder/GameStates/DisplayPeople_GS.cs
@@ -1,21 +1,39 @@
-﻿using RoystonGame.TV.DataModels;
+﻿using RoystonGame.TV.ControlFlows;
+using RoystonGame.TV.DataModels;
 using RoystonGame.TV.DataModels.Enums;
 using RoystonGame.TV.DataModels.GameStates;
+using RoystonGame.TV.DataModels.UserStates;
 using RoystonGame.TV.GameModes.BriansGames.BodyBuilder.DataModels;
 using RoystonGame.Web.DataModels.Enums;
 using RoystonGame.Web.DataModels.Requests;
+using RoystonGame.Web.DataModels.Responses;
 using RoystonGame.Web.DataModels.UnityObjects;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace RoystonGame.TV.GameModes.BriansGames.BodyBuilder.GameStates
 {
     public class DisplayPeople_GS : GameState
     {
+        private static UserPrompt PartyLeaderSkipButton(User user) => new UserPrompt()
+        {
+            Title = "Skip Reveal",
+            SubmitButton = true
+        };
         public DisplayPeople_GS(Lobby lobby, RoundTracker roundTracker, BodyBuilderConstants.DisplayTypes displayType, List<Setup_Person> peopleList, Action<User, UserStateResult, UserFormSubmission> outlet = null) : base(lobby, outlet)
         {
+            UserState partyLeaderState = new SimplePromptUserState(PartyLeaderSkipButton);
+            WaitingUserState waitingState = new WaitingUserState();
+
+            UserStateTransition waitForLeader = new WaitForPartyLeader(
+                lobby: this.Lobby,
+                outlet: this.Outlet,
+                partyLeaderPrompt: partyLeaderState,
+                waitingState: waitingState);
+
+            this.Entrance = waitForLeader;
+
             var unityImages = new List<UnityImage>();
             string title = null;
             if (displayType == BodyBuilderConstants.DisplayTypes.PlayerHands)
@@ -25,7 +43,7 @@ namespace RoystonGame.TV.GameModes.BriansGames.BodyBuilder.GameStates
                 {          
                     unityImages.Add(new UnityImage
                     {
-                        Base64Pngs = new StaticAccessor<IReadOnlyList<string>> {Value = roundTracker.AssignedPeople[user].GetOrderedDrawings() },
+                        Base64Pngs = new StaticAccessor<IReadOnlyList<string>> { Value = roundTracker.AssignedPeople[user].GetOrderedDrawings() },
                         BackgroundColor = new StaticAccessor<IReadOnlyList<int>> { Value = new List<int> { 255, 255, 255 } },
                         SpriteGridWidth = new StaticAccessor<int?> { Value = 1 },
                         SpriteGridHeight = new StaticAccessor<int?> { Value = 3 },
