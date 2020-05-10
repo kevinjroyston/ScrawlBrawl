3a1ceef299e10c616f7f51556d20df0bb7c9fd2d
Brian <brian@royston.com>
2020-05-10 14:18:15 -0700

    mothersday changes

--- TV/Extensions/StateExtensions.cs
+++ TV/Extensions/StateExtensions.cs
@@ -1,5 +1,9 @@
 ﻿using RoystonGame.TV.DataModels;
 using System;
+using Connector = System.Action<
+    RoystonGame.TV.DataModels.User,
+    RoystonGame.TV.DataModels.Enums.UserStateResult,
+    RoystonGame.Web.DataModels.Requests.UserFormSubmission>;
 
 namespace RoystonGame.TV.Extensions
 {
@@ -8,11 +12,19 @@ namespace RoystonGame.TV.Extensions
     /// </summary>
     public static class StateExtensions
     {
+        public static void Transition(this StateOutlet A, Connector B)
+        {
+            A.SetOutlet(B);
+        }
         public static void Transition(this StateOutlet A, StateInlet B)
         {
-            A.SetOutlet(B.Inlet);
+            A.Transition(B.Inlet);
         }
         public static void Transition(this StateOutlet A, Func<StateInlet> B )
+        {
+            A.Transition(() => B().Inlet);
+        }     
+        public static void Transition(this StateOutlet A, Func<Connector> B)
         {
             A.AddStateEndingListener(() =>
             {
