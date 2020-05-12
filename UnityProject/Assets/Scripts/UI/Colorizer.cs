70a581467730915045b5cebddbb894a0edb1986c
kevinjroyston <41594315+kevinjroyston@users.noreply.github.com>
2020-05-12 01:35:11 -0700

    Major fixes to unity client (#17)

---
+++ UnityProject/Assets/Scripts/UI/Colorizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Colorizer : MonoBehaviour
+{
+    public List<Color> OptionalColors;
+    public Image TargetImage;
+
+    public void RefreshColor(string identifier)
+    {
+        if (!string.IsNullOrWhiteSpace(identifier) && TargetImage != null && OptionalColors != null && OptionalColors.Count > 0)
+        {
+            TargetImage.color = OptionalColors[PositiveMod(identifier.GetHashCode(), OptionalColors.Count)];
+        }
+    }
+
+    private int PositiveMod(int a, int b)
+    {
+        return ((a % b) + b) % b;
+    }
+}
