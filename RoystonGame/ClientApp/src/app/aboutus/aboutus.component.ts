2888654994f6f78ab282493e922f4c54682d1202
PeteRoyston <66926642+PeteRoyston@users.noreply.github.com>
2020-09-15 00:16:12 -0700

    Rudimentary design of a landing page and about us page. (#83)

---
+++ RoystonGame/ClientApp/src/app/aboutus/aboutus.component.ts
@@ -0,0 +1,15 @@
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Component({
+  selector: 'homepage',
+  templateUrl: './aboutus.component.html',
+  styleUrls: ['./aboutus.component.css']
+})
+export class AboutUsComponent {
+
+  constructor() {
+       }
+
+}
