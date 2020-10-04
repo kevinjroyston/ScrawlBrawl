203ae85d494f5940d0ab6db59d462d1c1fdfbada
Brian Royston <broyston@berkeley.edu>
2020-10-03 19:28:15 -0700

    Started basics for game logogs

--- UnityProject/Assets/Scripts/UI/GameIconHandler.cs
+++ UnityProject/Assets/Scripts/UI/GameIconHandler.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameIconHandler : MonoBehaviour
 {
+    public Sprite ScrawlBrawlLogo = null;
+    public Sprite BodyBuilderLogo = null;
+    public Sprite BodySwapLogo = null;
+    public Sprite ImposterSyndromeLogo = null;
+    public Sprite ChaoticCooperationLogo = null;
+    public Sprite MimicLogo = null;
+    public Image IconHolder = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (IconHolder != null)
+        {
+            if (ScrawlBrawlLogo != null)
+            {
+                IconHolder.sprite = ScrawlBrawlLogo;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetGame() //TODO
     {
-        
+
     }
 }
