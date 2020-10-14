37367094d53d9a299d334551e257f31acbd4b162
Kevin Royston <kevin@royston.com>
2020-10-14 00:08:06 -0700

    fully specified config

--- src/WebFrontEnd/ClientApp/src/environments/environment.prod.ts
+++ src/WebFrontEnd/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     enableMsal: true,
+    overrideUrlsWithBrowserHost: false,
     backendApiUrl: "https://api.scrawlbrawl.tv/",
     frontendUrl: "https://scrawlbrawl.tv/"
 };
--- src/WebFrontEnd/ClientApp/src/environments/environment.staging.ts
+++ src/WebFrontEnd/ClientApp/src/environments/environment.staging.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     enableMsal: true,
+    overrideUrlsWithBrowserHost: false,
     backendApiUrl: "https://api.test.scrawlbrawl.tv/",
     frontendUrl: "https://test.scrawlbrawl.tv/"
 };
