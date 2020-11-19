25875cb3545bd967a4b290eca7c4807445be6c17
PeteRoyston <66926642+PeteRoyston@users.noreply.github.com>
2020-11-18 22:22:31 -0800

    Plr mergeassets (#127)
    * refactoring of game asset directive and folders
    
    * removed unneeded folder
    
    * removed duplicate call to load asset

--- src/WebFrontEnd/ClientApp/src/app/shared/components/gameassetdirective.component.ts
+++ src/WebFrontEnd/ClientApp/src/app/shared/components/gameassetdirective.component.ts
@@ -1,37 +1,53 @@
 import { Directive, ElementRef, HostListener, forwardRef, Input, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+/*
+Usage:
+  The root folder for DIV (html) game assets is /assets/GameAssets/
+  The root folder for IMG (svg!) game assets is /assets/GameAssets/game-images/
+  gameAssetClass is used to specify a SUB-FOLDER under the root level
+  gameAssetType is used to specify the end of the filename - defaults to "logo" for images and "description" for text
+   format: (root folder)/[gameAssetClass]/[gameAssetId]-[gameAssetType].[html|svg]
+   example: <div gameAsset [gameAssetClass]="'homepage-previews'" [gameAssetID]="'Mimic'">
+    would load:  /assets/GameAssets/homepage-previews/Mimic-description.html
+
+  Currently valid gameAssetClass (folders)
+    homepage-previews  (text)
+    lobby-descriptions  (text)
+    
+*/
 
 @Directive({
     selector: '[gameAsset]',
 })
 
 export class GameAssetDirective {
-  private _gameID: string;
-  @Input() descriptionPreview: boolean = false;
-  @Input() isSVG: boolean = false;
-  @Input() set gameID(value: string) { this._gameID = value; this.loadGame() }
-            get gameID(): string { return this._gameID}
+  private _gameAssetID: string;
+  @Input() gameAssetClass: string;
+  @Input() gameAssetType: string;
+  @Input() set gameAssetID(value: string) { this._gameAssetID = value; this.loadGameAsset() }
+            get gameAssetID(): string { return this._gameAssetID}
   http: HttpClient;
   element;
 
   constructor(http: HttpClient, element: ElementRef) {
-    console.log("Instantiating gameAsset " + element.nativeElement.nodeName + this.gameID);
+    console.log("Instantiating gameAsset " + element.nativeElement.nodeName);
     this.element = element.nativeElement;
     this.http = http;
   }
   
   ngOnInit() {
-    console.log("OnInit gameAsset " + this.gameID);
-    this.loadGame();
+    // console.log("OnInit gameAsset " + this.gameAssetID);  no longer needed due to setter on gameAssetID
+    // this.loadGameAsset();
   }
 
-  loadGame() {
+  loadGameAsset() {
+    console.log("gameAsset load " + this.gameAssetID);
 
     if (this.element.nodeName == 'IMG') {
-      this.element.src = this.determineAssetDestination();
+      this.element.src = this.determineImageAssetDestination();
     }
     else {
-      const url = this.determineDescriptionDestination() + this.gameID + "-description.html";
+      const url = this.determineDescriptionDestination();
       this.http.get(url, { observe: "body", responseType: "text" }).subscribe(
         data => {
           this.element.innerHTML = data;
@@ -39,17 +55,12 @@ export class GameAssetDirective {
     }
   }
 
-  determineAssetDestination = () => {
-
-    if (this.isSVG) {
-      return '/assets/svgs/gamemode/' + this.gameID + '.svg';
-    } 
-
-    return '/assets/GameAssets/game-images/' + this.gameID + "-logo.png";
+  determineImageAssetDestination = () => {
+    return '/assets/GameAssets/game-images/'+(this.gameAssetClass ? this.gameAssetClass+'/' : '') + this.gameAssetID + "-" 
+       + (this.gameAssetType ? this.gameAssetType : 'logo') + ".svg";
   }
 
   determineDescriptionDestination = () => {
-    const gameAssetFolder = '/assets/GameAssets/'
-    return gameAssetFolder + (this.descriptionPreview ? '/homepage-previews/' : '/lobby-descriptions/')
+    return '/assets/GameAssets/'+(this.gameAssetClass ? this.gameAssetClass+'/' : '') + this.gameAssetID + "-" + (this.gameAssetType ? this.gameAssetType : 'description') + ".html";
   } 
 }
