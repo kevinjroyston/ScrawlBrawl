511410786865326c6c89e11c449c8a80456fc897
Daniel Lee <daniellee0228@berkeley.edu>
2020-12-06 10:46:08 -0800

    refactor

--- src/WebFrontEnd/ClientApp/src/app/core/core.module.ts
+++ src/WebFrontEnd/ClientApp/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ThemeService, StorageService } from './services/theme.service';
+import { ThemeService } from './services/theme.service';
+import { StorageService } from './services/storage.service' 
 
 export function themeFactory(themeService: ThemeService) {
   return () => themeService.setThemeInit();
@@ -15,7 +16,12 @@ export function themeFactory(themeService: ThemeService) {
   providers: [
     ThemeService,
     StorageService,
-    {provide: APP_INITIALIZER, useFactory: themeFactory, deps: [ThemeService], multi: true},
+    {
+      provide: APP_INITIALIZER, 
+      useFactory: themeFactory, 
+      deps: [ThemeService], 
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
--- src/WebFrontEnd/ClientApp/src/app/core/services/theme.service.ts
+++ src/WebFrontEnd/ClientApp/src/app/core/services/theme.service.ts
@@ -1,19 +1,5 @@
 import { Injectable } from "@angular/core";
-
-@Injectable({
-    providedIn: 'root'
-})
-export class StorageService {
-    public get = (key: string): string => {
-        return localStorage ? localStorage.getItem(key) : null;
-    }
-    
-    public set = (key: string, value: any): void => {
-        if (localStorage) {
-            localStorage.setItem(key, value);
-        }
-    }
-}
+import { StorageService } from './storage.service'
 
 @Injectable({
     providedIn: 'root'
