57ac04f79422647cb91f108616d7f0ab7398869e
PeteRoyston <66926642+PeteRoyston@users.noreply.github.com>
2020-12-09 21:02:13 -0800

    added game info popup to learn more buttons on home page (#150)

--- src/WebFrontEnd/ClientApp/src/app/modules/home/components/gamemodes-section/gamemode/gamemode.component.ts
+++ src/WebFrontEnd/ClientApp/src/app/modules/home/components/gamemodes-section/gamemode/gamemode.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import {GameInfoDialogComponent} from '../../../../lobby/components/gameinfo-dialog/gameinfo-dialog.component';
+import GameModes from '@core/models/gamemodes'
 
 
 @Component({
@@ -9,11 +12,26 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GamemodeComponent implements OnInit {
 
   @Input() gameModeID: string;
+  @Input() gameId: number;
+  @Input() gameModes: GameModes.GameModeMetadata[];
   svgMapping: {}
 
-  constructor() { 
+  constructor(private matDialog: MatDialog)
+  {
+
 //    this.svgMapping = temporarySVGMapping;
   }
+  openGameInfoDialog = (event, game: number) => {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      gameModes: this.gameModes,
+      selectedGameMode: this.gameId,
+      proceedToGameSettings: null,
+    }
+    this.matDialog.open(GameInfoDialogComponent, dialogConfig);
+    event.stopPropagation();
+}
+
 
   ngOnInit() {
   }
