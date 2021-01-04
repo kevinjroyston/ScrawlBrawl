76ca3829ada638eb4a1d725a9e1d0c19abcd4bcb
Pete Royston <pete@royston.com>
2021-01-04 15:23:57 -0800

    #SB-98 mimic does a better job of messaging during waiting states

--- src/Backend/Games/KevinsGames/Mimic/GameStates/DisplayOriginal_GS.cs
+++ src/Backend/Games/KevinsGames/Mimic/GameStates/DisplayOriginal_GS.cs
@@ -7,6 +7,7 @@ using Backend.GameInfrastructure.ControlFlows.Exit;
 using Backend.Games.Common.DataModels;
 using Common.DataModels.Enums;
 using Backend.GameInfrastructure;
+using Backend.GameInfrastructure.DataModels.States.UserStates;
 
 namespace Backend.Games.KevinsGames.Mimic.GameStates
 {
@@ -15,7 +16,7 @@ namespace Backend.Games.KevinsGames.Mimic.GameStates
         public DisplayOriginal_GS(Lobby lobby, TimeSpan? displayTimeDuration, UserDrawing displayDrawing)
             : base(lobby,
                   stateTimeoutDuration: displayTimeDuration,
-                  exit: new WaitForStateTimeoutDuration_StateExit())
+                  exit: new WaitForStateTimeoutDuration_StateExit(waitingPromptGenerator:Prompts.DisplayText(Prompts.Text.LookAtTheScreen)))
         {
             this.Legacy_UnityView = new Legacy_UnityView(this.Lobby)
             {
