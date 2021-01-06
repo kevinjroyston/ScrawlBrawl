a33bbb22cd6253bb76beed709ac0921d7355cc06
PeteRoyston <66926642+PeteRoyston@users.noreply.github.com>
2021-01-06 00:15:06 -0800

    Plr aboutuswork (#181)
    * #SB-98 mimic does a better job of messaging during waiting states
    
    * work on about us page page SB-95 and social buttons SB-71

---
+++ src/WebFrontEnd/ClientApp/src/app/shared/components/svgs/socials/reddit.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'reddit-icon',
+    templateUrl: '../../../../../../src/assets/svgs/socials/reddit.svg',
+    styleUrls: ['../svg.component.scss']
+})
+export class RedditIcon {
+
+}
