8d03b3e15b1ef404c2aaff191c66d570522b2dc4
PeteRoyston <66926642+PeteRoyston@users.noreply.github.com>
2021-01-13 20:28:38 -0800

    using official brand icons for socials (#194)
    * using offical brand icons for social, fixed colored line on sliders
    
    * renamed poorly labeled class
    
    * fixed scrawl brawl button outlines in dark mode
    
    * after leave lobby, navigate to play page

--- src/WebFrontEnd/ClientApp/src/app/modules/user/pages/user/user-management.component.ts
+++ src/WebFrontEnd/ClientApp/src/app/modules/user/pages/user/user-management.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router'; 
 import { API } from '@core/http/api';
 import { MsalService, BroadcastService } from '@azure/msal-angular';
 import Galleries from '@core/models/gallerytypes';
@@ -16,7 +17,7 @@ export class UserManagementComponent {
     galleryTypes = [...Galleries.galleryTypes]; /* needed so html page can see it */
     currentDrawingType = this.galleryTypes[0].drawingType;
     
-    constructor(@Inject(API) private api: API)
+    constructor(@Inject(API) private api: API, private router: Router)
     {
         
     }
@@ -25,6 +26,7 @@ export class UserManagementComponent {
         await this.api.request({ type: "User", path: "Delete" }).subscribe({
             next: async data => {
                 console.log("Left lobby");
+                this.router.navigate(['/play'])
             },
             error: async (error) => {
                 console.error(error);
