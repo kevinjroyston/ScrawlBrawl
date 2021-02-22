5a0fd7ef39ad4ca76ef456884aa4c762c3d347b5
Kevin Royston <41594315+kevinjroyston@users.noreply.github.com>
2021-02-21 17:16:45 -0800

    Disable authentication for lobby management (#206)

--- src/WebFrontEnd/ClientApp/src/environments/environment.staging.ts
+++ src/WebFrontEnd/ClientApp/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    enableMsal: true,
+    enableMsal: false,
     overrideUrlsWithBrowserHost: false,
     backendApiUrl: "https://api.test.scrawlbrawl.tv/",
     frontendUrl: "https://test.scrawlbrawl.tv/",
