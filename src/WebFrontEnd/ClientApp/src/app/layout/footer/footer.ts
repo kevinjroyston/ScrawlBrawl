009d61155fab914ccc08d4ffec96c76a6d36ca54
Malodik <74339396+Malodik@users.noreply.github.com>
2021-03-07 19:09:35 -0500

    updated faq and trailer (#208)
    * updated faq and trailer
    
    * Update footer.ts
    
    * Update src/WebFrontEnd/ClientApp/src/app/layout/footer/footer.ts
    
    Co-authored-by: Kevin Royston <41594315+kevinjroyston@users.noreply.github.com>

--- src/WebFrontEnd/ClientApp/src/app/layout/footer/footer.ts
+++ src/WebFrontEnd/ClientApp/src/app/layout/footer/footer.ts
@@ -1,11 +1,11 @@
 export const footerLinksOrder = ['Questions?', 'About', 'Have thoughts?']
 
 export const socialLinks = {
-  discord: "https://discord.gg/HwjmTxx",
+  discord: "https://discord.gg/b2TMsQF7NG",
   reddit:  "https://www.reddit.com/r/ScrawlBrawl/",
   twitch: "https://www.twitch.tv/scrawlbrawl",
   twitter: "https://twitter.com/ScrawlBrawlGame",
-  youtube: "https://youtube.com/ScrawlBrawlGame", /* needs correct link */
+  youtube: "https://www.youtube.com/channel/UCXaY71hm9tbl1xSb-FKfZRA",
 }
 
 export const footerLinks = {
