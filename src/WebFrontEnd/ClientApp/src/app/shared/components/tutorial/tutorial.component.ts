2092626b11834e227a4d9e2ea8ec196fd07ef5f8
PeteRoyston <66926642+PeteRoyston@users.noreply.github.com>
2021-05-18 18:12:54 -0700

    initial code for game tutorials (#220)

--- src/WebFrontEnd/ClientApp/src/app/shared/components/tutorial/tutorial.component.ts
+++ src/WebFrontEnd/ClientApp/src/app/shared/components/tutorial/tutorial.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, Input, OnInit } from '@angular/core';
 import GameplayPrompts from '@core/models/gameplay';
 
 @Component({
   selector: 'app-tutorial',
   templateUrl: './tutorial.component.html',
-  styleUrls: ['./tutorial.component.scss']
+  styleUrls: ['./tutorial.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TutorialComponent implements OnInit {
   @Input() gameModeIdString: string;
