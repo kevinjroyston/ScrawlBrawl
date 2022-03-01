a589ebea61b3f3e8f8c9d5189532b29c091bc214
Kevin Royston <41594315+kevinjroyston@users.noreply.github.com>
2022-02-28 23:19:20 -0800

    Revert "Kev nodesasversionbug (#268)"
    This reverts commit e1c9902f2e51f7014751c05c6deb5878dfbc5089.

--- src/WebFrontEnd/ClientApp/src/app/modules/home/components/opensource-section/opensource-section.component.spec.ts
+++ src/WebFrontEnd/ClientApp/src/app/modules/home/components/opensource-section/opensource-section.component.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
